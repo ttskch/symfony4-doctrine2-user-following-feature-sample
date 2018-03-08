6547a6191f89e620134b5362bebe303c73de8d36
Takashi Kanemoto <kanemoto.takashi@gmail.com>
2018-03-08 22:31:05 +0900

    create User entity

---
+++ src/Entity/User.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ *
+ * @ORM\Table(uniqueConstraints={@ORM\UniqueConstraint(name="unique_username", columns={"username"})})
+ */
+class User
+{
+    /**
+     * @var int
+     *
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(type="string", length=255)
+     */
+    public $username;
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+}
